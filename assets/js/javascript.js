5f0aeacc4a6de3f5fc8832b7c018edd27a7dae24
Gustavo Duque <gustavo.duque616@gmail.com>
2023-12-05 14:19:16 -0800

    added README.MD

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -9,48 +9,48 @@ var now = moment().format("H A");
 $("#currentDay").text(today);
 
 
-/* planWorkday entries for each hour of the workday */
-var planWorkday = [
-    { time: "9 AM", 
+/* workdayplanner entries for each hour of the workday */
+var workdayplanner = [
+    { time: "9 AM 🔅", 
         event: "" },
-    { time: "10 AM", 
+    { time: "10 AM🔅", 
         event: "" },
-    { time: "11 AM", 
+    { time: "11 AM🔅", 
         event: "" },
-    { time: "12 PM", 
+    { time: "12 PM🔅", 
         event: "" },
-    { time: "1 PM", 
+    { time: "1 PM 🔅", 
         event: "" },
-    { time: "2 PM", 
+    { time: "2 PM 🔅", 
         event: "" },
-    { time: "3 PM", 
+    { time: "3 PM 🔅", 
         event: "" },
-    { time: "4 PM", 
+    { time: "4 PM 🌅", 
         event: "" },
-    { time: "5 PM", 
+    { time: "5 PM 🌅", 
         event: "" },
   ];
 
 /* Local Storage check */
-var workEvents = JSON.parse(localStorage.getItem("workDay"));
-if (workEvents) {
-  planWorkday = workEvents;
+var planned = JSON.parse(localStorage.getItem("workDay"));
+if (planned) {
+	workdayplanner = planned;
 }
 
 /* Current Day */
 $("#currentDay").text(today);
 
 /* Create rows */
-planWorkday.forEach(function(timeBlock, index) {
+workdayplanner.forEach(function(timeBlock, index) {
 	var timeLabel = timeBlock.time;
-	var blockColor = colorRow(timeLabel);
+	var colorBlock = colorRow(timeLabel);
 	var row =
 		'<div class="time-block" id="' +
 		index +
 		'"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
 		timeLabel +
 		'</div><textarea class="form-control ' +
-		blockColor +
+		colorBlock +
 		'">' +
 		timeBlock.event +
 		'</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
@@ -62,10 +62,10 @@ planWorkday.forEach(function(timeBlock, index) {
 /* Color rows based on current time */
 function colorRow(time) {
 	var planNow = moment(now, "H A");
-	var planEntry = moment(time, "H A");
-	if (planNow.isBefore(planEntry) === true) {
+	var entry = moment(time, "H A");
+	if (planNow.isBefore(entry) === true) {
 		return "future";
-	} else if (planNow.isAfter(planEntry) === true) {
+	} else if (planNow.isAfter(entry) === true) {
 		return "past";
 	} else {
 		return "present";
@@ -85,8 +85,8 @@ $(".saveBtn").on("click", function() {
 			.siblings("textarea")
 			.val()
 	);
-	planWorkday[blockID].event = userEntry;
+	workdayplanner[blockID].event = userEntry;
 
 	/* Set local storage */
-	localStorage.setItem("workDay", JSON.stringify(planWorkday));
+	localStorage.setItem("workDay", JSON.stringify(workdayplanner));
 });
